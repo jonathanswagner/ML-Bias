fb01571554bf97162f9970e988cc8f3b1df61124
Jonathan <jonathanswagner@gmail.com>
2020-03-21 09:33:16 +0800

    added second metric

--- code/operators.py
+++ code/operators.py
@@ -1,21 +1,33 @@
 
 import pandas as pd
 import numpy as np
-from aif360.metrics import BinaryLabelDatasetMetric
+from aif360.metrics import BinaryLabelDatasetMetric, ClassificationMetric
 from aif360.algorithms.preprocessing import Reweighing
 from aif360.datasets import BinaryLabelDataset
 
 def get_disparity_index(di):
     return 1 - np.minimum(di, 1 / di)
 
-def calc_disparity_index(data, target_variable, protected_variable):
+def calc_disparity_index(data, target_variable, protected_variable, privileged_input, unprivileged_input):
     df_aif = BinaryLabelDataset(df=data, label_names=[target_variable],
                                 protected_attribute_names=[protected_variable])
-    privileged_group = [{protected_variable: 1}] #male=1
-    unprivileged_group = [{protected_variable: 0}] #female=0
+    privileged_group = [{protected_variable: privileged_input}] #male=1
+    unprivileged_group = [{protected_variable: unprivileged_input}] #female=0
     metric_orig = BinaryLabelDatasetMetric(df_aif, unprivileged_group, privileged_group)
     print('1-min(DI, 1/DI):', get_disparity_index(metric_orig.disparate_impact()).round(3))
     if get_disparity_index(metric_orig.disparate_impact()).round(3) < 0.2:
+        print('The algorithm can be considered to be not biased')
+    else:
+        print('There is a potential bias')
+
+def calc_stat_parity(data, target_variable, protected_variable, privileged_input, unprivileged_input):
+    df_aif = BinaryLabelDataset(df=data, label_names=[target_variable],
+                                protected_attribute_names=[protected_variable])
+    privileged_group = [{protected_variable: privileged_input}] #male=1
+    unprivileged_group = [{protected_variable: unprivileged_input}] #female=0
+    metric_orig = BinaryLabelDatasetMetric(df_aif, unprivileged_group, privileged_group)
+    print(metric_orig.statistical_parity_difference().round(3))
+    if metric_orig.statistical_parity_difference().round(3) > -0.1:
         print('The algorithm can be considered to be not biased')
     else:
         print('There is a potential bias')
